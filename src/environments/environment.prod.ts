e534f3773b906c3207f79caeefcf9ac44fabb633
dmarquezg <7danmg7@gmail.com>
2020-11-06 15:57:02 -0600

    Conexión a base de datos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDuXgO9fW5A2QUtSLRijjeqvlSYzZYmf-w",
+    authDomain: "reservahabitacion-473ca.firebaseapp.com",
+    databaseURL: "https://reservahabitacion-473ca.firebaseio.com",
+    projectId: "reservahabitacion-473ca",
+    storageBucket: "reservahabitacion-473ca.appspot.com",
+    messagingSenderId: "213829045665",
+    appId: "1:213829045665:web:8b83d52cc100792da78fe0",
+    measurementId: "G-DEMD2X2HY7"
+  }
 };
