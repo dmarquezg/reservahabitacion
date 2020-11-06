66bed3f22105e61a03f048e3a55e99ef3fc5c718
dmarquezg <7danmg7@gmail.com>
2020-11-06 16:20:45 -0600

    CRUD in service

--- src/app/services/data-db.service.ts
+++ src/app/services/data-db.service.ts
@@ -1,19 +1,47 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-import { MessageI } from '../models/message.interface';
+import { StudentI } from '../models/message.interface';
+import { map } from 'rxjs/operators';
+import 'firebase/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataDbService {
-  private reservaCollection: AngularFirestoreCollection<MessageI>;
+  private reservaCollection: AngularFirestoreCollection<StudentI>;
+  private reservas: Observable<StudentI[]>;
+  private reservaDoc: AngularFirestoreDocument<StudentI>;
+
   constructor(private afs: AngularFirestore) {
-    this.reservaCollection = afs.collection<MessageI>('registro_inquilinos');
+    this.reservaCollection = afs.collection<StudentI>('registro_inquilinos');
+    this.reservas = this.reservaCollection.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as StudentI;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    );
+  }
+
+  getReservas() {
+    return this.reservas;
+  }
+
+  saveMessage(reserva: StudentI): void{
+    this.reservaCollection.add(reserva);
+  }
+
+  deleteReserva(reserva: StudentI) {
+    this.reservaDoc = this.afs.doc(`registro_inquilinos/${reserva.id}`);
+    this.reservaDoc.delete();
+    console.log('Delete reservation', reserva);
+  }
+
+  updateReserva(reserva: StudentI) {
+    this.reservaDoc = this.afs.doc(`registro_inquilinos/${reserva.id}`);
+    this.reservaDoc.update(reserva);
+    //console.log('Update reservation', reserva);
   }
   
-
-  saveMessage(newContact: MessageI): void{
-    this.reservaCollection.add(newContact);
-  }
 }
